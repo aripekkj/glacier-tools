794a8260f1c7f73c65bae80dd1818943cad9496d
aripekkj <31698615+aripekkj@users.noreply.github.com>
2020-10-08 19:40:06 +0300

    Add files via upload

--- scripts/oggm_get_user_centerlines.py
+++ scripts/oggm_get_user_centerlines.py
@@ -5,10 +5,10 @@ Created on Tue Sep 29 17:47:44 2020
 
 Notes:
     1.10. 2020
-    Creates centerlines on user imported shape. Not sure about DEM
+    Creates centerlines on user imported glacier outlines and DEM. 
     
     1.10.2020
-    Seems to work with python 3.6 and latest OGGM build
+    Seems to work with python 3.6 and latest OGGM build (MacOS)
     
     29.9.2020
     When usign the latest build of OGGM and python 3.7:
@@ -28,37 +28,40 @@ import oggm
 from oggm import cfg, utils, workflow, tasks, graphics
 from oggm.core import gis
 from pyproj import CRS
-
 from functools import partial, wraps
 from shapely.ops import transform as shp_trafo
 from salem import wgs84
-
+from rgitools.funcs import compute_intersects
 
 cfg.initialize(logging_level='WORKFLOW')
 cfg.PARAMS['border'] = 10
 
 
 # out fp
-out_fp = r'/Users/apj/Documents/_HY/Greenland/centerlines/centerlines_50s_edited.shp'
+out_fp = r'/Users/apj/Documents/_HY/Greenland/centerlines/edited_glacier_divides/centerlines_50s_edited_08102020.shp'
 
 # read dem
 custom_dem_path = r'/Users/apj/Documents/_HY/Greenland/demcoreg_result/study_area_dems_coregistered/50s_dem_wgs84.tif'
 custom_dem_path
 
+# set custom dem path to PATHS, so it can be used in tasks. tasks.define_glacier_region parameter source='user' will read dem from cfg.PATHS['dem_file']
 cfg.PATHS['dem_file'] = custom_dem_path
 
 # do not use intersects
-cfg.PARAMS['use_intersects'] = False
+cfg.PARAMS['use_intersects'] = True
 
 # read shapefile
-fp = r'/Users/apj/Documents/_HY/Greenland/outlines/05_rgi60_studyarea_50s_extent_wgs84_edited.shp'
+fp = r'/Users/apj/Documents/_HY/Greenland/outlines/edited_glacier_divides/05_rgi60_studyarea_50s_extent_wgs84_edited.shp'
+
 entity = gpd.read_file(fp)
 
 # test with one glacier
-#entity = entity[entity['RGIId'] == 'RGI60-05.02208_2']
+#entity = entity[entity['RGIId'] == 'RGI60-05.02319_1']
 
 cfg.PATHS['working_dir'] = utils.gettempdir('user', reset=True)
 gdirs = workflow.init_glacier_directories(entity)
+
+gdir = gdirs[0]
 
 
 # tasks to be executed
@@ -76,15 +79,13 @@ for task in list_tasks:
     # The order matters!
     workflow.execute_entity_task(task, gdirs)
 
-
 # plot. suitable for plotting one glacier
 #f, ax = plt.subplots()
-#da_user = xr.open_rasterio(gdirs.get_filepath('dem'))
+#da_user = xr.open_rasterio(gdirs[0].get_filepath('dem'))
 #da_user.plot(cmap='terrain', ax=ax);
 #gdirs.read_shapefile('outlines').plot(ax=ax, color='none', edgecolor='black');
 #graphics.plot_centerlines(gdirs, use_flowlines=False)
-
-
+#graphics.plot_catchment_areas(gdirs)
 
 # get centerlines
 olist = []
